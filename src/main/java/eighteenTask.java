76f3db278c0e8e608b893cf1500d5bd474bd6976
Максим <max08041993@mail.ru>
2021-01-02 10:43:39 +0300

    New

--- src/main/java/eighteenTask.java
+++ src/main/java/eighteenTask.java
@@ -26,5 +26,4 @@ public class eighteenTask {
         }
         System.out.println(string);
     }
-
 }
---
+++ src/main/java/nineteenTask.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class nineteenTask {
+    public static void main(String[] args) {
+        System.out.println("19. Найдите количество вхождения 'aba' в строку.");
+        Scanner scanner = new Scanner(System.in);
+        char[] strings = scanner.nextLine().toCharArray();
+        int count=0;
+        for (int i=0;i<strings.length-2;i++){
+            if (strings[i]=='a'&strings[i+1]=='b'&strings[i+2]=='c')
+                count++;
+        }
+        System.out.println("Количество вхождений 'abc' в строке состовляет: "+count);
+    }
+}
