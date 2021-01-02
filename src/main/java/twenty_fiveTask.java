c2b43f886a20aa490d5781bd0c1278d69b31c37f
Максим <max08041993@mail.ru>
2021-01-02 21:35:08 +0300

    New

---
+++ src/main/java/twenty_fiveTask.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class twenty_fiveTask {
+    public static void main(String[] args) throws IOException {
+        System.out.println("25. Дан текст. Найдите наибольшее количество подряд идущих пробелов в нем.");
+        String string = Files.lines(Paths.get("text.txt")).reduce("",String::concat);
+        char[] var=string.toCharArray();
+        System.out.println(var);
+        int count=0;
+        int result=0;
+        for (char c : var) {
+            if (c == ' ') {
+                count++;
+                System.out.println(count);
+                if (count > result) {
+                    result = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        System.out.println("Наибольшее количество пробелов между словами в строке составляет: " + result);
+    }
+}
