a27fdcfe097d31fc3bf5b33ccc949074df1981ec
Максим <max08041993@mail.ru>
2021-01-07 20:58:57 +0300

    New

--- src/main/java/Strings/twenty_sexTask.java
+++ src/main/java/Strings/twenty_sexTask.java
@@ -3,10 +3,22 @@ package Strings;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class twenty_sexTask {
     public static void main(String[] args) throws IOException {
-        System.out.println("26. ");
-        String string = Files.lines(Paths.get("text.txt")).reduce("",String::concat);
+        System.out.println("26. Даны два слова. Найдите только те символы слов, которые встречаются в обоих словах только один раз.");
+        String[] string = Files.lines(Paths.get("text.txt")).reduce("",String::concat).split(" ");
+        ArrayList<String> aaa = new ArrayList<String>(Arrays.asList(string));
+        ArrayList<Character> simOne = new ArrayList<Character>();
+        for (String bb:aaa){
+            for (char vv:bb.toCharArray()){
+                if (!simOne.contains(vv)){
+                    simOne.add(vv);
+                }
+            }
+        }
+        System.out.println(simOne);
     }
 }
